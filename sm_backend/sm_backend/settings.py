3fd8adcd079a71f8eed17c9411381a9374df7ab6
Jean Munoz <devjeanmunoz@gmail.com>
2024-02-21 19:51:26 -0500

    cors and postgress upgrade

--- sm_backend/sm_backend/settings.py
+++ sm_backend/sm_backend/settings.py
@@ -1,5 +1,9 @@
+import os
 from datetime import timedelta
 from pathlib import Path
+
+# Import dj-database-url at the beginning of the file.
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,12 +13,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$pfs6e%9&h&x+r^s^*%!lwyt^)-8#txdmol6z+y49j^swe^m5c'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-$pfs6e%9&h&x+r^s^*%!lwyt^)-8#txdmol6z+y49j^swe^m5c')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = ['jean-social-media.onrender.com']
+ALLOWED_HOSTS = ['jean-social-media.onrender.com','127.0.0.1']
 
 WEBSITE_URL='https://jean-social-media.onrender.com'
 
@@ -79,6 +84,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,11 +122,8 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': dj_database_url.config(default='postgres://admin:bIG4erHX6622GGZzHkJ4S3eRXu3zNjwt@dpg-cnb942ed3nmc73cucc40-a.oregon-postgres.render.com/guakia',conn_max_age=600)
     }
-}
 
 
 # Password validation
@@ -158,6 +161,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL= 'media/'
 MEDIA_ROOT=BASE_DIR / 'media'
 # Default primary key field type
