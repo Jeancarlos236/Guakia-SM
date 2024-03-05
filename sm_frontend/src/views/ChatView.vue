2cf5045840d8babe3c297d3209df836293836b87
Jean Munoz <devjeanmunoz@gmail.com>
2024-03-05 18:01:41 -0500

    websocket updated to wss

--- sm_frontend/src/views/ChatView.vue
+++ sm_frontend/src/views/ChatView.vue
@@ -148,7 +148,7 @@
 			setActiveConversation(conversation) {
 				this.activeConversation = conversation;
 				this.socket = new WebSocket(
-					`ws://localhost:8000/ws/chat/${conversation.id}/`,
+					`wss://jean-social-media.onrender.com/ws/chat/${conversation.id}/`,
 				);
 				this.socket.onmessage = (event) => {
 					const message = JSON.parse(event.data);
