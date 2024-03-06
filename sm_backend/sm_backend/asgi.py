f84f196977ba62555549f25de9758a91c4c20048
Jeffrey Soriano <jeffreysoriano5@gmail.com>
2024-03-06 01:56:25 -0400

    added missing django.setup

--- sm_backend/sm_backend/asgi.py
+++ sm_backend/sm_backend/asgi.py
@@ -1,12 +1,18 @@
 # asgi.py
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sm_backend.settings')
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
 from chat.consumers import ChatConsumer
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sm_backend.settings')
+
+
 application = ProtocolTypeRouter(
     {
         "websocket": AuthMiddlewareStack(
